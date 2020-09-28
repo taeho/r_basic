4f4fbbc4db4e95bc751c58a4ed379e68b028f618
gopae02 <gopae02@gmail.com>
2020-09-28 23:30:35 +0900

    # r basic test

---
+++ chap02/test1.R
@@ -0,0 +1,13 @@
+# Title     : r basic step 02
+# Objective : r basic
+# Created by: user
+# Created on: 2020-09-28
+
+# test1.r
+# 변수에 값 저장 <- 기호 사용
+a <- 10
+b <- 5
+print(a %/% b)
+
+# 2의 10승
+print(2^10)
